ebbdc190c21a0b5ab399b4b91ee3db342b1f256d
Almir Rogério Camolesi <camolesi@fema.edu.br>
2026-08-10 17:07:11 -0300

    Atualização Inicial
    Atualização Inicial

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using CRUDALNBCC.Models;
+
+namespace CRUDALNBCC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Aluno> Alunos {get; set;}
+
+        
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CRUDALNBCC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
